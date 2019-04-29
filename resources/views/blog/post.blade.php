fa447230ae34e94aa295d85b2c4350901594e2e9
U1710040 <azibonu19@gmail.com>
2019-04-29 14:59:38 +0500

    lab

---
+++ resources/views/blog/post.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+@section('content')
+<div>
+	<h1>Learning Laravel</h1>
+	<p>hjksfagfdhjdasbnmvcxzn shlkFHKYUIOWEQ nmbvdfajlkdfsuoew ,nmzbcziopqwp sdmn,dfs,nmrewkjl.</p>
+@endsection
